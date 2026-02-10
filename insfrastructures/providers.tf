3fc02ad0ad0eff58cc93d30153b1a2ee2fb41f3e
Vu The Huy <vuthehuy.t61@gmail.com>
2026-02-10 22:44:22 +0700

    feat: Add Terraform backend configuration and resources for S3 state and DynamoDB locking.

--- insfrastructures/providers.tf
+++ insfrastructures/providers.tf
@@ -1,11 +1,20 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  # Uncomment this block AFTER Phase 1 (bootstrapping resources)
+  backend "s3" {
+    bucket         = "fastapi-ecs-demo-terraform-state-654654329682"
+    key            = "state/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    dynamodb_table = "fastapi-ecs-demo-terraform-locks"
   }
 }
 
